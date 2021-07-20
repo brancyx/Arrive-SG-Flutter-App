dc653f9af56ad24d05c83de6618afe9454ac8b88
BRANDON <brundon.cyx@gmail.com>
2021-07-20 21:52:48 +0800

    Added Gesture Detector to Read Guide

--- lib/screens/home/components/read_guide.dart
+++ lib/screens/home/components/read_guide.dart
@@ -26,65 +26,75 @@ class ReadGuide extends StatelessWidget {
           height: 320.0,
           child: Column(
             children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: getProportionateScreenWidth(kDefaultPadding),
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 50,
-                      width: 250,
-                      child: Text(
-                        "How many hours do Singaporeans work every month?",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontSize: getProportionateScreenWidth(13),
-                        ),
-                      ),
-                    ),
-                    Spacer(),
-                    ClipRRect(
-                          borderRadius: BorderRadius.circular(20.0),
-                          child: Image(
-                            height: 70.0,
-                            width: 70.0,
-                            image: AssetImage('assets/images/work.png'),
-                            fit: BoxFit.cover,
+              GestureDetector(
+                onTap: () {
+                  print("Container clicked");
+                },
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(kDefaultPadding),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 250,
+                        child: Text(
+                          "How many hours do Singaporeans work every month?",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: getProportionateScreenWidth(13),
                           ),
                         ),
-                  ],
+                      ),
+                      Spacer(),
+                      ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: Image(
+                              height: 70.0,
+                              width: 70.0,
+                              image: AssetImage('assets/images/work.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                    ],
+                  ),
                 ),
               ),
               VerticalSpacing(of:20),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: getProportionateScreenWidth(kDefaultPadding),
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 50,
-                      width: 250,
-                      child: Text(
-                        "What is the average family spending per week",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontSize: getProportionateScreenWidth(13),
-                        ),
-                      ),
-                    ),
-                    Spacer(),
-                    ClipRRect(
-                          borderRadius: BorderRadius.circular(20.0),
-                          child: Image(
-                            height: 70.0,
-                            width: 70.0,
-                            image: AssetImage('assets/images/economy.png'),
-                            fit: BoxFit.cover,
+              InkWell(
+                onTap: () {
+                  print("Second Container clicked");
+                },
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(kDefaultPadding),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 250,
+                        child: Text(
+                          "What is the average family spending per week",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: getProportionateScreenWidth(13),
                           ),
                         ),
-                  ],
+                      ),
+                      Spacer(),
+                      ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: Image(
+                              height: 70.0,
+                              width: 70.0,
+                              image: AssetImage('assets/images/economy.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                    ],
+                  ),
                 ),
               ),
               VerticalSpacing(of: 20,),
